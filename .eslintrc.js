6cef97f83ea48c43a492b65bc2fd9e5996c9dec6
jbargu <jure.grabnar@gmail.com>
2021-06-18 20:57:10 +0200

    Update prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,31 @@
 // Copyright 2017-2021 @polkadot/ui authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-const base = require('@polkadot/dev/config/eslint.cjs');
+const base = require("@polkadot/dev/config/eslint.cjs");
 
 module.exports = {
   ...base,
   ignorePatterns: [
-    '.eslintrc.js',
-    '.github/**',
-    '.vscode/**',
-    '.yarn/**',
-    '**/build/*',
-    '**/coverage/*',
-    '**/node_modules/*',
-    '**/*.d.ts'
+    ".eslintrc.js",
+    ".github/**",
+    ".vscode/**",
+    ".yarn/**",
+    "**/build/*",
+    "**/coverage/*",
+    "**/node_modules/*",
+    "**/*.d.ts"
   ],
   parserOptions: {
     ...base.parserOptions,
-    project: [
-      './tsconfig.json'
-    ]
+    project: ["./tsconfig.json"]
   },
   rules: {
     ...base.rules,
     // add override for any (a metric ton of them, initial conversion)
-    '@typescript-eslint/no-explicit-any': 'off',
+    "@typescript-eslint/no-explicit-any": "off",
     // these should be removed, there are 8 of them as errors
-    '@typescript-eslint/no-non-null-assertion': 'off',
+    "@typescript-eslint/no-non-null-assertion": "off",
     // this seems very broken atm, false positives
-    '@typescript-eslint/unbound-method': 'off'
+    "@typescript-eslint/unbound-method": "off"
   }
 };
